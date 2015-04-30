d457fe06b4824bdce01514c05a958a28e68a941c
Emir <emiratesbp@gmail.com>
2015-04-30 13:41:24 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,17 @@
+<?php
+	$adresa[0]="12082 Montgomery Rd."; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Cincinnati OH, 45249";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="USA";
+	$telephone[0]=5135838897;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="3883 Rutherford Road"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Vaughan ON, L4L 9R8";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="CANADA";
+	$telephone[1]=9052659988;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=2;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
